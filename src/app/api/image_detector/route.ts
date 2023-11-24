1d333db6cf3d0a19229afad5c6e346cf27bfd081
YogaNovvaindra <yoga.november2000@gmail.com>
2023-11-24 18:34:11 +0700

    image detector api

---
+++ src/app/api/image_detector/route.ts
@@ -0,0 +1,40 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+//create get esp function
+export async function GET() {
+  try {
+    const image_detector = await db.image_detector.findMany({
+    });
+    if (image_detector.length === 0) {
+      return NextResponse.json({ message: "image_detector not found!" }, { status: 404 });
+    }
+    return NextResponse.json(image_detector, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong!" },
+      { status: 500 }
+    );
+  }
+}
+
+//create post esp function
+export async function POST(req: Request) {
+  try {
+    const { label, confidence} = await req.json();
+
+    const image_detector = await db.image_detector.create({
+      data: {
+        label: label,
+        confidence: confidence,
+      },
+    });
+    return NextResponse.json(image_detector, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong!" + error },
+      { status: 500 }
+    );
+  }
+}
